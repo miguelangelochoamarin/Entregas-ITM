23383997195c2658d8223d42cbdcd607f339b402
MIGUEL ANGEL OCHOA MARIAN <miguelochoa1113948@correo.itm.edu.co>
2026-02-22 14:37:26 -0500

    se agrega la clase CommisionEmployee

---
+++ OPP01/OPPconcepts.Backend/CommisionEmployee.cs
@@ -0,0 +1,59 @@
+﻿namespace OPPconcepts.Backend;
+public class CommisionEmployee : Employee
+{
+    //Fields
+    private float _commisionPercent;
+
+    public CommisionEmployee(float commisionPercent, decimal sales)
+    {
+        _commisionPercent = commisionPercent;
+        Sales = sales;
+    }
+
+    private decimal _sales;
+
+
+    //Constructor
+
+    public CommisionEmployee(int id, string firtsName, string lastName, bool isActive, Date bornDay, Date hireDay, float commisionPercent, decimal sales) : 
+        base(id, firtsName, lastName, isActive, bornDay, hireDay)
+    {
+
+    }
+    
+    //Properties
+    public float CommisionPercent
+    {
+        get => _commisionPercent;
+        set => _commisionPercent = ValidateCommisionPercent(value);
+    }
+    public decimal Sales
+    { 
+        get => _sales;
+        set => _sales = ValidateSales(value);
+    }
+    //Methods
+    public override decimal GetvalueToPay() => (decimal)CommisionPercent * Sales;
+
+
+    public override string ToString() => base.ToString() + $"\n\t" +
+        $"Commision percent: {CommisionPercent,20:P2}\n\t" +
+        $"Sales......... {Sales,20:C2}";
+
+    private float ValidateCommisionPercent(float commisionPercent)
+    {
+        {
+            if (CommisionPercent < 0 || commisionPercent > 1)
+                throw new ArgumentOutOfRangeException(nameof(commisionPercent), " Commision Percent must be betwen 0 and 1.");
+        }
+        return CommisionPercent;
+    }
+    private decimal ValidateSales(decimal sales) 
+    { 
+        if(sales<0)
+
+            { throw new ArgumentOutOfRangeException(nameof(sales), "Sales must be greater than or equal to 0.");
+        }
+        return sales;   
+    }
+}
--- OPP01/OPPconcepts.frontend/Program.cs
+++ OPP01/OPPconcepts.frontend/Program.cs
@@ -6,12 +6,16 @@ try
     decimal payroll = 0;
     var employee1 = new SalaryEmployee(1010,"Maria","Perez", true, new Date(1990, 5, 15), new Date(2020, 1, 1), 2500000);
     employees.Add(employee1);   
-    var employee2 = new SalaryEmployee(2010, "Joaquin", "Gonzalez", true, new Date(1980, 5, 15), new Date(2020, 1, 1), 10395876);
+    var employee2 = new SalaryEmployee(2020, "Joaquin", "Gonzalez", true, new Date(1980, 5, 15), new Date(2020, 1, 1), 10395876);
     employees.Add(employee2);  
     var employee3 = new HourlyEmployee(2030, "Ana", "Lopez", true, new Date(1980, 5, 15), new Date(2020, 1, 1), 18000,95);
     employees.Add(employee3);
-    var employee4 = new HourlyEmployee(2030, "Kevin", "Cardona", true, new Date(1980, 5, 15), new Date(2020, 1, 1), 10000, 112);
+    var employee4 = new HourlyEmployee(2040, "Kevin", "Cardona", true, new Date(1980, 5, 15), new Date(2020, 1, 1), 10000, 112);
     employees.Add(employee4);
+    var employee5 = new CommisionEmployee(2050, "Sofia", "Gomez", true, new Date(1988, 2, 18), new Date(2001, 1, 1), 0.03f, 600000000);
+    employees.Add(employee5);
+    var employee6 = new CommisionEmployee(2060, "Diego", "Martinez", true, new Date(1985, 9, 9), new Date(2013, 1, 1), 0.03f, 120000000);
+    employees.Add(employee6);
     foreach ( var employee in employees )
     {       
         Console.WriteLine(new string('-',47));

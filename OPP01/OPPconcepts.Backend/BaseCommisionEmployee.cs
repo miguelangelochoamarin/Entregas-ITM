54112efa58712e4ba568cb068879efae2fe0e6ec
MIGUEL ANGEL OCHOA MARIAN <miguelochoa1113948@correo.itm.edu.co>
2026-02-22 16:21:14 -0500

    finaliza calseBase comision empleado

---
+++ OPP01/OPPconcepts.Backend/BaseCommisionEmployee.cs
@@ -0,0 +1,42 @@
+﻿namespace OPPconcepts.Backend;
+
+public class BaseCommisionEmployee: CommisionEmployee
+{
+    //Fields
+    private decimal _salary;
+
+    //Constructors
+    public BaseCommisionEmployee(int id, string firtsName, string lastName, bool isActive, Date bornDay, Date hireDay, float commisionPercent, decimal sales, decimal salary) :
+        base(id, firtsName, lastName, isActive, bornDay, hireDay, commisionPercent, sales)
+    {
+        Salary = salary;
+    }
+
+
+
+
+
+    //Properties
+    public decimal Salary
+    { 
+        get => _salary; 
+        set => _salary =ValidateSalary(value);
+    }
+
+
+    //Method
+
+    public override decimal GetvalueToPay()=>  base.GetvalueToPay() + Salary;
+
+    public override string ToString() =>  $"{base.ToString()} " + $"\n\tSalary baase...:(Salary,20:C2)";
+    
+    
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 500000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(salary), "Salary base must be greater than or equal to $2.000.000.");
+        }
+        return salary;
+    }
+}
--- OPP01/OPPconcepts.frontend/Program.cs
+++ OPP01/OPPconcepts.frontend/Program.cs
@@ -16,6 +16,13 @@ try
     employees.Add(employee5);
     var employee6 = new CommisionEmployee(2060, "Diego", "Martinez", true, new Date(1985, 9, 9), new Date(2013, 1, 1), 0.03f, 120000000);
     employees.Add(employee6);
+    var employee7 = new BaseCommisionEmployee(2070, "Laura", "garcia", true, new Date(1972, 5, 24), new Date(2015, 2, 3), 0.015f,2500000000, 550000);
+    employees.Add(employee7);
+    var employee8 = new BaseCommisionEmployee(2080, "Sofia", "garcia",    true, new Date(1972, 5, 24), new Date(2015, 2, 3), 0.015f, 0, 550000);
+    employees.Add(employee8);
+
+
+
     foreach ( var employee in employees )
     {       
         Console.WriteLine(new string('-',47));

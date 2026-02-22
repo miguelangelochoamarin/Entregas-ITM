7a0b48600db14b06afff2f8f7ffc66063f86bd3a
MIGUEL ANGEL OCHOA MARIAN <miguelochoa1113948@correo.itm.edu.co>
2026-02-21 20:02:36 -0500

    se agraga clase  HourlyEmployee

---
+++ OPP01/OPPconcepts.Backend/HourlyEmployee.cs
@@ -0,0 +1,55 @@
+﻿namespace OPPconcepts.Backend
+{
+    public class HourlyEmployee:Employee
+    {
+        //fields
+        private decimal _hourlyValue;
+        private float _workingHours;
+
+        //Constructors
+
+        public HourlyEmployee(int id, string firtsName, string lastName, bool isActive, Date bornDay, Date hireDay, decimal hourlyValue = 0, float workingHours = 0) :
+            base(id, firtsName, lastName, isActive, bornDay, hireDay)
+        {
+            HourlyValue = hourlyValue;
+            WorkingHours = workingHours;
+        }
+
+        //Properties
+        public decimal HourlyValue 
+        {
+            get => _hourlyValue; 
+            set => _hourlyValue = ValidateHourlyValue(value); 
+        }
+        public float WorkingHours 
+        { 
+            get => _workingHours; 
+            set => _workingHours = ValidateWorkingHours (value); 
+        }
+
+        //Methods
+        public override decimal GetvalueToPay() =>(decimal)WorkingHours * HourlyValue;
+        
+
+        public override string ToString() => base.ToString() + $"\n\t"+ 
+            $"Hourly Value: {HourlyValue,20:C2}\n\t" + 
+            $"Working Hours: {WorkingHours,20:N2}";
+
+        private decimal ValidateHourlyValue(decimal hourlyValue)
+        {
+            if (hourlyValue < 10000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hourlyValue), "Hourly Value must be greater than or equal to $10.000.");
+            }
+            return hourlyValue;
+        }
+        private float ValidateWorkingHours(float workingHours)
+        {
+            if (workingHours < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(workingHours), "Working Hours must be greater than or equal to 0.");
+            }
+            return workingHours;
+        }
+    }
+}
--- OPP01/OPPconcepts.frontend/Program.cs
+++ OPP01/OPPconcepts.frontend/Program.cs
@@ -4,9 +4,11 @@ try
 {
     var employee1 = new SalaryEmployee(1010,"Maria","Perez", true, new Date(1990, 5, 15), new Date(2020, 1, 1), 2500000);
     var employee2 = new SalaryEmployee(2010, "Joaquin", "Gonzalez", true, new Date(1980, 5, 15), new Date(2020, 1, 1), 10395876);
+    var employee3 = new HourlyEmployee(2030, "Ana", "Lopez", true, new Date(1980, 5, 15), new Date(2020, 1, 1), 1500,160);
 
     Console.WriteLine(employee1);
     Console.WriteLine(employee2);
+    Console.WriteLine(employee3);
 }
 catch   (Exception ex)
 {
